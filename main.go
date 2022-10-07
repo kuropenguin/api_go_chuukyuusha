47892b0f29870d93c080172a5c130fbd0cacc53a
kuropenguin <kuro1994.t@gmail.com>
2022-10-07 23:26:49 +0900

    modify handlers

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/kuropenguin/api_go_chuukyuusha/handlers"
+)
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world\n")
+	}
+}
 
 func main() {
-	fmt.Println("println")
+
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
+
+	log.Println("server start at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
