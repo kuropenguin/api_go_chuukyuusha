6de6abc9fc289aab23bce4e78a62a50a27f537a9
kuropenguin <kuro1994.t@gmail.com>
2022-10-13 22:48:33 +0900

    add gorrila mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kuropenguin/api_go_chuukyuusha
 
 go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,24 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/kuropenguin/api_go_chuukyuusha/handlers"
 )
 
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
-
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world\n")
-	}
-}
-
 func main() {
 
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
