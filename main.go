4268331112dc33878965a494f4a17bdbd83db872
kuropenguin <kuro1994.t@gmail.com>
2022-11-17 22:44:06 +0900

    select 1 row

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/kuropenguin/api_go_chuukyuusha/models"
 )
 
 func main() {
@@ -20,11 +21,32 @@ func main() {
 	}
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
-		fmt.Println("ping err")
+	articleID := 10
+	const sqlStr = `
+	select *
+	from articles
+	where article_id = ?
+	;
+	`
+	row := db.QueryRow(sqlStr, articleID)
+	// if err := row.Err(); err != nil {
+	// fmt.Println(err)
+	// return
+	// }
+
+	var createdTime sql.NullTime
+	var article models.Article
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	if err != nil {
+		fmt.Println("fmt")
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
 	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	fmt.Printf("%+v\n", article)
 
 }
