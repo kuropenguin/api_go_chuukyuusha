583f836577ea1384033d5f0f43c6992d61eccbc5
kuropenguin <kuro1994.t@gmail.com>
2022-11-24 22:10:15 +0900

    add test

--- repositories/articles_test.go
+++ repositories/articles_test.go
@@ -23,32 +23,77 @@ func TestSelectArticleDetail(t *testing.T) {
 	}
 	defer db.Close()
 
-	expected := models.Article{
-		ID:       1,
-		Title:    "firstPost",
-		Contents: "this is my first blog",
-		UserName: "saki",
-		NiceNum:  2,
+	tests := []struct {
+		testTitle string
+		expected  models.Article
+	}{
+		{
+			testTitle: "subtest1",
+			expected: models.Article{
+				ID:       1,
+				Title:    "firstPost",
+				Contents: "this is my first blog",
+				UserName: "saki",
+				NiceNum:  2,
+			},
+		}, {
+			testTitle: "subtest2",
+			expected: models.Article{
+				ID:       1,
+				Title:    "firstPost",
+				Contents: "this is my first blog",
+				UserName: "saki",
+				NiceNum:  2,
+			},
+		},
 	}
 
-	got, err := repositories.SelectArticleDetail(db, expected.ID)
+	for _, test := range tests {
+		t.Run(test.testTitle, func(t *testing.T) {
+			got, err := repositories.SelectArticleDetail(db, test.expected.ID)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got.ID != test.expected.ID {
+				t.Errorf("ID: get %d but want %d\n", got.ID, test.expected.ID)
+			}
+			if got.Title != test.expected.Title {
+				t.Errorf("Title: get %s but want %s\n", got.Title, test.expected.Title)
+			}
+			if got.Contents != test.expected.Contents {
+				t.Errorf("Content: get %s but want %s\n", got.Contents, test.expected.Contents)
+			}
+			if got.UserName != test.expected.UserName {
+				t.Errorf("UserName: get %s but want %s\n", got.UserName, test.expected.UserName)
+			}
+			if got.NiceNum != test.expected.NiceNum {
+				t.Errorf("NiceNum: get %d but want %d\n", got.NiceNum, test.expected.NiceNum)
+			}
+		})
+	}
+
+}
+
+// SelectArticleList関数のテスト
+func TestSelectArticleList(t *testing.T) {
+	// テストで使うデータベースに接続
+	dbUser := "docker"
+	dbPassword := "docker"
+	dbDatabase := "sampledb"
+	dbConn := fmt.Sprintf("%s:%s@tcp(db-for-go)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if got.ID != expected.ID {
-		t.Errorf("ID: get %d but want %d\n", got.ID, expected.ID)
+	defer db.Close()
+	// テスト対象の関数を実行
+	expectedNum := 2
+	got, err := repositories.SelectArticleList(db, 1)
+	if err != nil {
+		t.Fatal(err)
 	}
-	if got.Title != expected.Title {
-		t.Errorf("Title: get %s but want %s\n", got.Title, expected.Title)
-	}
-	if got.Contents != expected.Contents {
-		t.Errorf("Content: get %s but want %s\n", got.Contents, expected.Contents)
-	}
-	if got.UserName != expected.UserName {
-		t.Errorf("UserName: get %s but want %s\n", got.UserName, expected.UserName)
-	}
-	if got.NiceNum != expected.NiceNum {
-		t.Errorf("NiceNum: get %d but want %d\n", got.NiceNum, expected.NiceNum)
+	// SelectArticleList関数から得たArticleスライスの長さが期待通りでないならFAILにする
+	if num := len(got); num != expectedNum {
+		t.Errorf("want %d but got %d articles\n", expectedNum, num)
 	}
 }
